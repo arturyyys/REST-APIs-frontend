9eb6a667c52eea29c3691603ae0b953174390615
Artūras <arturyyys@gmail.com>
2024-10-20 13:00:50 +0300

    ErrorHandler

---
+++ src/components/ErrorHandler/ErrorHandler.js
@@ -0,0 +1,23 @@
+import React, { Fragment } from "react";
+
+import Backdrop from "../Backdrop/Backdrop";
+import Modal from "../Modal/Modal";
+
+// Renders an error modal and backdrop when an error occurs
+const errorHandler = (props) => (
+  <Fragment>
+    {props.error && <Backdrop onClick={props.onHandle} />}
+    {props.error && (
+      <Modal
+        title="An Error Occurred"
+        onCancelModal={props.onHandle}
+        onAcceptModal={props.onHandle}
+        acceptEnabled
+      >
+        <p>{props.error.message}</p>
+      </Modal>
+    )}
+  </Fragment>
+);
+
+export default errorHandler;
